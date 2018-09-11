7194ff53f80a4912bef409ff9e84264f10e825a2
Kathan Master <kathan.master@yahoo.com>
2018-09-11 15:07:22 -0400

    Removed commented out code

--- src/Main.py
+++ src/Main.py
@@ -30,7 +30,6 @@ for stock in listStocks:
     if count == 1499:
         requrl = requrl + '&types=quote,stats'
         r = requests.get(requrl).json()
-        print(r)
         parseResults(r)
         count = 0
         requrl = 'https://api.iextrading.com/1.0/stock/market/batch?symbols='
@@ -39,36 +38,7 @@ for stock in listStocks:
 
 requrl = requrl + '&types=quote,stats'
 r = requests.get(requrl).json()
-print(r)
 parseResults(r)
 
 
 print(result)
-
-# def getPBV(symbol):
-#     r = requests.get("https://api.iextrading.com/1.0/stock/" + symbol + "/stats").json()
-#     return r['priceToBook']
-#
-#
-# def getPE(symbol):
-#     r = requests.get("https://api.iextrading.com/1.0/stock/" + symbol + "/book").json()
-#     return r['quote']['peRatio']
-#
-#
-# # Iterate through each stock and calculate the P/E * P/BV value
-# # If it is below 22.5, add it to result list
-# for stock in listStocks:
-#     stockSymbol = stock['symbol']
-#     print('starting execution for ' + stockSymbol)
-#     stockName = stock["name"]
-#     stockPE = getPE(stockSymbol)
-#     if stockPE is None:
-#         continue
-#     stockPBV = getPBV(stockSymbol)
-#     if stockPBV is None:
-#         continue
-#     val = stockPE * stockPBV
-#     if val <= 22.5:
-#         result.append({"symbol": stockSymbol, "name": stockName, "val": val})
-#
-# print(result)
